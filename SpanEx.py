35b0b2aba3c9938202883eb7f3757cf380362b8f
Robert Deyeso <robert-deyeso@uiowa.edu>
2025-01-23 18:18:38 -0600

    SpanEx Improvements

--- SpanEx.py
+++ SpanEx.py
@@ -11,62 +11,111 @@ class SpanEx(Scene):
             faded_line_ratio=2,
         )
 
-        # Create Mobjects
-        v1 = np.array([1,-1,0])
-        c1 = ValueTracker(-4)
-        c1v1 = lambda: np.array([c1.get_value(), -1*c1.get_value(), 0])
-        v1_arrow=Arrow(
-            start=ORIGIN,
-            end=v1,
-            buff=0,
-            color=RED,
-            stroke_width=4,
+        # Mobjects
+        v1 = Vector([1,-1], color=GREEN)
+        v2 = Vector([-2, 0.5], color=YELLOW)
+        x1 = ValueTracker(1)
+        x2 = ValueTracker(1)
+        x1v1 = always_redraw(
+            lambda: Vector([x1.get_value(),-1*x1.get_value()], color=GREEN)
         )
-        c1v1_arrow=always_redraw(
-                lambda: Arrow(
-                start=ORIGIN,
-                end=c1v1(),
-                buff=0,
-                color=YELLOW,
-                stroke_width=3,
-            )
+        x2v2 = always_redraw(
+            lambda: Vector([-2*x2.get_value(), 0.5*x2.get_value()], color=YELLOW)
+        )
+        xvsum = always_redraw(
+            lambda: Vector([-2*x2.get_value(), 0.5*x2.get_value()], color=YELLOW).shift(Vector([x1.get_value(),-1*x1.get_value()], color=GREEN).get_end())  
         )
         span_v1 = Line(
-            start=-4*v1,
-            end=4*v1,
-            color=YELLOW,
+            start=-6*v1.get_unit_vector(),
+            end=6*v1.get_unit_vector(),
+            color=PURPLE,
             stroke_width=2,
         )
 
 
-        # Label the transformation matrix
-        v1_label = MathTex(r"\mathbf{v}_1=\begin{bmatrix} 1 \\ -1 \end{bmatrix}", color=RED).next_to(v1_arrow)
-        c1v1_label = always_redraw(
-            lambda: MathTex(r"c_1 \mathbf{v}", color=YELLOW).next_to(c1v1_arrow)
+        # Labels
+        v1_label = MathTex(r"\mathbf{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}", color=GREEN).next_to(v1.get_end(), RIGHT, buff=0.2)
+        x1v1_label = always_redraw(
+            lambda: MathTex(r"x_1 \mathbf{v}_2", color=GREEN).next_to(x1v1.get_end(), RIGHT, buff=0.2)
         )
-        #c1_label = always_redraw(
-            #lambda: MathTex(f"c_1 = {c1.get_value():.2f}", color=YELLOW).next_to(grid.c2p(2,2))
-        #)
-        span_label = MathTex(r"\text{Span}(\mathbf{v}_1)", color=YELLOW).next_to(v1_arrow)
+        v2_label = MathTex(r"\mathbf{v}=\begin{bmatrix} -2 \\ 0.5 \end{bmatrix}", color=YELLOW).next_to(v2.get_end(), RIGHT, buff=0.2)
+        x2v2_label = always_redraw(
+            lambda: MathTex(r"x_2 \mathbf{v}_2", color=YELLOW).next_to(x2v2.get_end(), RIGHT, buff=0.2)
+        )
+        #sum_label = MathTex(r"x_1 \mathbf{v}_1 + x_2 \mathbf{v}_2")
+        span_label = MathTex(r"\text{Span}(\mathbf{v}_1)", color=YELLOW).next_to(v1.get_end(), RIGHT, buff=0.2)
 
-        # Add the grid, label, and basis vectors to the scene
-        self.add(grid, v1_arrow, v1_label,)
+        # Initial Scene
+        self.add(grid)
+        self.wait()
 
-        # Animate
-        self.play(FadeIn(grid, v1_arrow, v1_label, run_time=1))
+        # Animations
+        self.play(
+            FadeIn(
+                v1, 
+                #v1_label, 
+                v2, 
+                #v2_label, 
+                run_time=1
+            )
+        )
         self.wait(1)
+
         self.play(
-            Create(c1v1_arrow),
-            FadeIn(c1v1_label), #c1_label),
-        )
-        self.play(c1.animate.set_value(10), run_time=8, rate_func=linear)
-        self.play(
-            FadeOut(c1v1_arrow, c1v1_label), #c1_label),
-            FadeIn(v1_arrow, v1_label),
-            Transform(v1_arrow, span_v1),
-            Transform(v1_label, span_label),
-            run_time=2,
+            v2.animate.shift(v1.get_end())
         )
 
-        # Hold the final scene
+        #self.play(
+            #FadeIn(
+                #x1v1, 
+                #x1v1_label, 
+                #x2v2, 
+                #x2v2_label
+            #)
+        #)
+        self.play(
+            FadeOut(v1, v2),
+            FadeIn(x1v1, xvsum)
+        )
+        self.play(
+            x1.animate.set_value(2), 
+            x2.animate.set_value(-1),
+            run_time=2,
+            rate_func=smooth
+        )
+        self.play(
+            x1.animate.set_value(1), 
+            x2.animate.set_value(-3),
+            run_time=2,
+            rate_func=smooth
+        )
+        self.play(
+            x1.animate.set_value(-1), 
+            x2.animate.set_value(1),
+            run_time=2,
+            rate_func=smooth
+        )
+        
+        #Swap v1 for Span(v1)
+        #self.play(
+            #FadeOut(
+                #x1v1, 
+                #x1v1_label
+            #),
+            #FadeIn(
+                #v1, 
+                #v1_label
+            #),
+            #Transform(
+                #v1, 
+                #span_v1
+            #),
+            #Transform(
+                #v1_label, 
+                #span_label
+            #),
+            #run_time=3,
+        #)
+
+        # Hold Final
         self.wait(3)
