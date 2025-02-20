5f10cbd657dd4dbd104b87af6d0604d37cb898dd
Robert Deyeso <robert-deyeso@uiowa.edu>
2025-02-20 14:37:54 -0600

    ddx_final

--- ddx.py
+++ ddx.py
@@ -13,6 +13,9 @@ class ddx(MovingCameraScene):
             return 1/3 * (t-1)**3 - 1/2 * (t-1) + 1
         def fprime(t):
             return (t-1)**2 - 1/2
+        def secant_slope(t):
+            return (f(t+1) - f(1)) / t
+        
 
         fgraph = axes.plot(f, color=BLUE, x_range=[-5, 5], stroke_width=4)
         
@@ -29,17 +32,17 @@ class ddx(MovingCameraScene):
         a_label_coord = Dot().move_to(axes.c2p(0,0)).set_opacity(0)
 
         # Labels
-        #FTC_text = MarkupText("Fundamental Theorem of Calculus part II").to_edge(UP)
-        title = MathTex(r"\text{The Derivative of} \, s \, \text{at} \, a").to_edge(UL).scale(0.75)
-        slope_def = MathTex(r"\text{Slope}", r"= \frac{s(b) - s(a)}{b-a}").next_to(title, DOWN).scale(0.75)
-        deriv_def = MathTex(r"s'(a) = \lim_{b \to a} \frac{s(b) - s(a)}{b-a}").next_to(title, 1.3*DOWN).scale(0.75).shift(0.25*RIGHT)
-        slope_def[0].set_color(RED)
+        title = MathTex(r"\text{The Derivative of} \,\, s \,\, \text{at} \,\, t=a").to_edge(UL).scale(0.75).shift(0.1*LEFT)
+        deriv_def = MathTex(r"s'(a)", r"= \lim_{b \to a} \frac{s(b) - s(a)}{b-a}").next_to(title, 1.3*DOWN).scale(0.75).shift(0.1*LEFT)
+        deriv_def[0].set_color(RED)
         start_label = MathTex(r"a").next_to(axes.c2p(1, 0), 1.2*DOWN)
         fstart_label = MathTex(r"s(a)").next_to(axes.c2p(0, f(1)), LEFT)
         end_label = MathTex(r"b").next_to(axes.c2p(3, 0), DOWN)
         fend_label = MathTex(r"s(b)").next_to(axes.c2p(0, f(3)), LEFT)
         graph_label = MathTex(r"s(t)", color=BLUE).next_to(axes.c2p(4, 3))
-        lim_label = MathTex(r"\text{Slide} \, b \to a").next_to(axes.c2p(3,2), DR).scale(0.75)
+        lim_label = MathTex(r"\text{Slide} \,\, b \to a").next_to(axes.c2p(3,2), DR).scale(0.75)
+        slope_def = MathTex(r"\text{Slope}", r"= \frac{s(b) - s(a)}{b-a}").next_to(end_label, DOWN, buff=-0.15).scale(0.6)
+        slope_def[0].set_color(RED)
 
         # Dashed Lines
         start_line = DashedLine(
@@ -52,8 +55,7 @@ class ddx(MovingCameraScene):
         # Scene
 
         self.play(
-            Write(title),
-            Create(axes), 
+            FadeIn(axes), 
             Create(fgraph),
             Write(graph_label),
         )
@@ -149,11 +151,11 @@ class ddx(MovingCameraScene):
 
         self.play(
             FadeIn(secant_line),
-            FadeOut(start_line, fstart_line, end_line, fend_line)
         )
         self.wait()
         self.play(
-            FadeOut(start_label, fstart_label, end_label, fend_label)
+            FadeOut(start_label, fstart_label, end_label, fend_label),
+            FadeOut(start_line, fstart_line, end_line, fend_line)
         )
         self.wait()
 
@@ -169,17 +171,32 @@ class ddx(MovingCameraScene):
         self.wait()
 
         self.play(
-            FadeIn(lim_label)
+            FadeIn(lim_label, start_label, start_line, end_line, end_label)
         )
         
-        fend_line.add_updater(
+        end_line.add_updater(
             lambda l: l.become(
-                
+                DashedLine(
+                    start = end_label.get_center()+0.2*UP, end = axes.c2p(1+dx_value.get_value(), f(1+dx_value.get_value())), color=WHITE, stroke_width=2, buff=0.1
+                )
             )
+        )
+        end_label.add_updater(
+            lambda l: l.become(
+                MathTex(r"b").next_to(axes.c2p(1+dx_value.get_value(), 0), DOWN)
+            )
+        )
+        slope_dyn = always_redraw(
+            lambda: MathTex(r"\text{Slope}", f"= {secant_slope(dx_value.get_value()):.2f}", color=RED).next_to(axes.c2p(3,-0.5), DOWN).scale(0.6)
+        )
+        slope_dyn[0].set_color(RED)
+        self.play(
+            ReplacementTransform(slope_def, slope_dyn)
         )
 
         self.play(
             dx_value.animate.set_value(0.001),
+            FadeOut(start_label, start_line, end_label, end_line),
             run_time=6,
             rate_func=smooth
         )
@@ -191,7 +208,10 @@ class ddx(MovingCameraScene):
             self.camera.frame.animate.scale(1/0.65).move_to(axes.get_center()),
         )
         self.play(
-            ReplacementTransform(slope_def, deriv_def)
+            ReplacementTransform(slope_dyn, deriv_def)
+        )
+        self.play(
+            Write(title)
         )
 
         self.wait(4)
