103177ae11c543673cee509719fe49778646f609
Robert DeYeso III <robldeye@gmail.com>
2025-08-13 10:30:14 -0500

    update setops

--- College Algebra/SetOps.py
+++ College Algebra/SetOps.py
@@ -6,13 +6,18 @@ class SetOps(Scene):
             width=4.0, height=5.0, fill_opacity=0.5, color=BLUE, stroke_width=10
         ).move_to(LEFT)
         ellipse2 = ellipse1.copy().set_color(color=RED).move_to(RIGHT)
-        set_ops_text = MarkupText("<u>Set Operation</u>").next_to(ellipse1, UP * 3)
-        ellipse_group = Group(set_ops_text, ellipse1, ellipse2).move_to(LEFT * 3)
-        self.play(FadeIn(ellipse_group))
+        ellipse_group = Group(ellipse1, ellipse2).to_edge(LEFT)
+        ellipse1_label = MathTex(r"A", color=BLUE).next_to(ellipse1, UP)
+        ellipse2_label = MathTex(r"B", color=RED).next_to(ellipse2, UP)
+        set_ops_text = MarkupText("<u>Set Operations</u>")
+        self.play(Write(set_ops_text), run_time=1)
+        self.play(set_ops_text.animate.to_corner(UL))
+        self.play(FadeIn(ellipse_group, ellipse1_label, ellipse2_label))
         self.wait()
 
         # Intersection
-        intersection_text = Text("Intersection", font_size=23).to_corner(UR)
+        intersection_text = MathTex(r"\text{Intersection: }", r"A \cap B").to_corner(UR)
+        intersection_text[1].set_color(GREEN)
         i = Intersection(ellipse1, ellipse2, color=GREEN, fill_opacity=0.5)
         self.play(Write(intersection_text))
         self.play(Indicate(i))
@@ -20,7 +25,8 @@ class SetOps(Scene):
         self.wait()
 
         # Union
-        union_text = Text("Union", font_size=23).next_to(i, DOWN)
+        union_text = MathTex(r"\text{Union: }", r"A \cup B").next_to(i, DOWN)
+        union_text[1].set_color(ORANGE)
         u = Union(ellipse1, ellipse2, color=ORANGE, fill_opacity=0.5)
         self.play(Write(union_text))
         self.play(Indicate(u))
@@ -28,11 +34,13 @@ class SetOps(Scene):
         self.wait()
 
         # Exclusion
-        exclusion_text = Text("Exclusion", font_size=23).next_to(u, DOWN)
-        e = Exclusion(ellipse1, ellipse2, color=YELLOW, fill_opacity=0.5)
-        self.play(Write(exclusion_text))
-        self.play(Indicate(e))
-        self.play(e.animate.scale(0.3).next_to(exclusion_text, DOWN))
-        self.wait()
+        difference_text = MathTex(r"\text{Difference: }", r"A - B").next_to(u, DOWN)
+        difference_text[1].set_color(YELLOW)
+        d = Difference(ellipse1, ellipse2, color=YELLOW, fill_opacity=0.5)
+        self.play(Write(difference_text))
+        self.play(Indicate(d))
+        self.play(d.animate.scale(0.3).next_to(difference_text, DOWN))
+        self.wait(2)
+
         
         
