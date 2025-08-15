e363413b1c5861d6437d203b934b885727ce488a
Robert DeYeso III <robldeye@gmail.com>
2025-08-15 16:42:59 -0500

    limitdef closer

--- Calculus I/Limit Def.py
+++ Calculus I/Limit Def.py
@@ -24,7 +24,9 @@ class LimitDef(Scene):
         inv_funcs = solve(Eq(y, f_expr), x)  # [-sqrt(y), sqrt(y)]
         g_y = inv_funcs[1] # Grabs sqrt(y)
 
-        curve = axes.plot(lambda x: f_num(x), x_range=[0, float(sqrt(y_max))], color=YELLOW)
+        f_label = MathTex(r"f(x) = x^2", color=BLUE).to_corner(UR)
+
+        curve = axes.plot(lambda x: f_num(x), x_range=[0, 2], color=BLUE)
         epsilon = ValueTracker(0.5*(L))
 
         # Function to build dynamic area region since get_area only works for integration wrt x
@@ -34,11 +36,13 @@ class LimitDef(Scene):
             y_axis_pts = [axes.c2p(0.0, yv) for yv in y_vals]
             poly = Polygon(
                 *y_axis_pts, *reversed(curve_pts),
-                color=BLUE, fill_opacity=0.55, stroke_width=0
+                color=YELLOW, fill_opacity=0.55, stroke_width=0
             )
             return poly
         y_poly = always_redraw(lambda: y_polygon(epsilon.get_value()))
-        epsilon_label = always_redraw(lambda: MathTex(rf"\epsilon = {epsilon.get_value():.2f}").to_edge(LEFT).align_to(title, LEFT))
+
+        epsilon_label = MathTex(rf"\epsilon = {epsilon.get_value():.2f}").to_edge(LEFT).align_to(title, LEFT)
+        epsilon_label.add_updater(lambda l: l.become(MathTex(rf"\epsilon = {epsilon.get_value():.2f}").to_edge(LEFT).align_to(title, LEFT)))
         epsilon_u = always_redraw(lambda: MathTex(rf"{L} + \epsilon").next_to(axes.c2p(0, L+epsilon.get_value()), LEFT))
         epsilon_d = always_redraw(lambda: MathTex(rf"{L} - \epsilon").next_to(axes.c2p(0, L-epsilon.get_value()), LEFT))
 
@@ -50,9 +54,10 @@ class LimitDef(Scene):
             )
         )
 
-        delta_label = always_redraw(lambda: MathTex(rf"\delta = {float(sqrt(epsilon.get_value())):.2f}").next_to(epsilon_label, DOWN).align_to(title, LEFT))
-        delta_r = always_redraw(lambda: MathTex(rf"{latex(nsimplify(L))} + \delta").next_to(axes.c2p(float(sqrt(L+epsilon.get_value())), 0), DOWN))
-        delta_l = always_redraw(lambda: MathTex(rf"{latex(nsimplify(L))} - \delta").next_to(axes.c2p(float(sqrt(L-epsilon.get_value())), 0), DOWN, buff=1))
+        delta_label = MathTex(rf"\delta = {float(sqrt(epsilon.get_value())):.2f}").next_to(epsilon_label, DOWN).align_to(title, LEFT)
+        delta_label.add_updater(lambda l: l.become(MathTex(rf"\delta = {float(sqrt(epsilon.get_value())):.2f}").next_to(epsilon_label, DOWN).align_to(title, LEFT)))
+        delta_r = always_redraw(lambda: MathTex(rf"{latex(nsimplify(sqrt(L)))} + \delta").next_to(axes.c2p(float(sqrt(L+epsilon.get_value())), 0), DOWN))
+        delta_l = always_redraw(lambda: MathTex(rf"{latex(nsimplify(sqrt(L)))} - \delta").next_to(axes.c2p(float(sqrt(L-epsilon.get_value())), 0), DOWN, buff=1))
 
         self.play(Write(title), run_time=1)
         self.play(title.animate.to_corner(UL))
@@ -62,37 +67,41 @@ class LimitDef(Scene):
         self.wait()
 
         self.play(Create(curve))
+        self.play(FadeIn(f_label))
         self.wait()
 
         limitdef_1 = MathTex(r"\text{Q) How do we formalize } f(x) \to L \text{ as } x \to a\text{ ?}").scale(0.6).next_to(title, DOWN).align_to(title, LEFT)
-        limitdef_2 = MathTex(r"\text{We need two things}:").scale(0.6).next_to(limitdef_1, DOWN).align_to(title, LEFT)
-        limitdef_3 = MathTex(r"1) \text{Whenever the distance } |f(x) - L| \text{ decreases...}").scale(0.6).next_to(limitdef_2, DOWN).align_to(title, LEFT)
-        limitdef_4 = MathTex(r"2) \text{We want to be sure the distance} |x - a| \text{ also decreases.}").scale(0.6).next_to(limitdef_3, DOWN).align_to(title, LEFT)
+        limitdef_2 = MathTex(r"""
+                             \text{A) Whenever the distance } |f(x) - L| \text{ decreases,} \\
+                             \quad \text{We need the distance } |x - a| \text{ to also decrease.}
+                             """).scale(0.6).next_to(limitdef_1, 2*DOWN).align_to(title, LEFT)
+        limitdef_f = MathTex(r"""
+                             \text{A) For any } \epsilon \text{ so that } |f(x) - L| < \epsilon, \\
+                             \quad \text{There is a } \delta \text{ so that } |x - a | < \delta.
+                             """).scale(0.6).next_to(limitdef_1, 2*DOWN).align_to(title, LEFT)
 
         self.play(Write(limitdef_1), run_time=1)
         self.wait(2)
-        self.play(Write(limitdef_2), run_time=1)
 
         self.play(FadeIn(y_poly))
         self.play(Write(epsilon_label), Write(epsilon_u), Write(epsilon_d), run_time=1)
-        self.play(Write(limitdef_3), run_time=1)
-        self.wait()
+        self.play(Write(limitdef_2))
+        self.wait(2)
+        self.play(Transform(limitdef_2, limitdef_f))
+        self.wait(2)
 
-        self.play(epsilon.animate.set_value(0.1), run_time=2)
+        self.play(epsilon.animate.set_value(0.1), run_time=1)
         self.wait()
-        self.play(epsilon.animate.set_value(1), run_time=2)
+        self.play(epsilon.animate.set_value(1), run_time=1)
         self.wait()
-
-        # insert limit def text at this stage
         
-        self.play(Write(limitdef_4), run_time=1)
         self.wait()
         self.play(FadeIn(x_poly))
         self.play(Write(delta_label), Write(delta_r), Write(delta_l), run_time=1)
         self.wait()
 
-        self.play(epsilon.animate.set_value(0.1), run_time=2)
+        self.play(epsilon.animate.set_value(0.1), run_time=3)
         self.wait()
-        self.play(epsilon.animate.set_value(1), run_time=2)
-        self.wait()
+        self.play(epsilon.animate.set_value(1), run_time=3)
+        self.wait(3)
 
